2897436f270fd870928174a65641b3cf1db268c0
Amirul A. Ali <amirul.alis@gmail.com>
2010-09-02 22:31:05 +0800

    * moved scripts into wp_enqueue_script * added option between self-hosted prototype or google's api
    Signed-off-by: Amirul A. Ali <amirul.alis@gmail.com>

--- addicted_live_search/live_search.php
+++ addicted_live_search/live_search.php
@@ -9,15 +9,18 @@ Author URI: http://addictedtonew.com/
 */
 
 function addicted_add() {
-	echo '<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/prototype/1.6/prototype.js"></script>' . "\n";
-	echo '<script type="text/javascript" src="' . get_bloginfo('siteurl') . '/wp-content/plugins/addicted_live_search/live_search.js"></script>' . "\n";
-	echo '
-	<script type="text/javascript">
-		AddictedToLiveSearch.url = "' . get_bloginfo('siteurl') . '/wp-content/plugins/addicted_live_search/search_results.php";
-	</script>';
+	echo '<script type="text/javascript">AddictedToLiveSearch.url = "' . get_bloginfo('siteurl') . '/wp-content/plugins/addicted_live_search/search_results.php";</script>';
 }
 
 add_action('wp_footer', 'addicted_add');
+/*
+Using Google hosted script
+wp_deregister_script('prototype');
+wp_enqueue_script('prototype', 'http://ajax.googleapis.com/ajax/libs/prototype/1.6/prototype.js');
+*/
+wp_enqueue_script('prototype');
+wp_enqueue_script('live_search', '/wp-content/plugins/addicted_live_search/live_search.js');
+
 
 function addicted_search_rewrite($wp_rewrite) {
 	$rules = array('wp-content/plugins/addicted_live_search/search_results.php' => '/');
